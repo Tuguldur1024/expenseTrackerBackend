2c18648a149914780425e56cb75589cd6e62d254
Tuguldur1024 <purevochirbilguuntuguldur@gmail.com>
2024-09-30 09:17:54 +0800

    first

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`app is running at http://localhost:${port}`);
+});
