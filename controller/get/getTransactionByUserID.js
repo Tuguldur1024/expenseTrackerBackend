a0c66f0393bb47058069d423e2f1398fde950a50
Tuguldur1024 <purevochirbilguuntuguldur@gmail.com>
2024-10-04 09:04:11 +0800

    push

--- controller/get/getTransactionByUserID.js
+++ controller/get/getTransactionByUserID.js
@@ -1,7 +1,7 @@
 import { sql } from "../../database";
 
 export const getTransactionByUserId = async (request, response) => {
-  const { userid } = request.body;
+  const { user_id } = request.body;
   console.log(request.body);
   try {
     const myTransactions =
@@ -9,7 +9,7 @@ export const getTransactionByUserId = async (request, response) => {
     from transactions 
     inner join users on users.id = transactions.user_id
     inner join categories on categories.id = transactions.category_id
-    WHERE transactions.user_id = ${userid}`;
+    WHERE transactions.user_id = ${user_id}`;
     response.status(200).json({ transactions: myTransactions });
   } catch (error) {
     response.status(400).json({ message: error });
--- controller/get/signIn.js
+++ controller/get/signIn.js
@@ -2,6 +2,7 @@ import { sql } from "../../database";
 
 export const signIn = async (request, response) => {
   const { email, password } = request.body;
+  console.log(request.body);
   try {
     const user =
       await sql`SELECT * FROM users WHERE email=${email} AND password=${password}`;
