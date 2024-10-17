092a8d87e860710caf6fba5bf1ca412db33ce058
Tuguldur1024 <purevochirbilguuntuguldur@gmail.com>
2024-10-17 08:31:46 +0800

    morning push

--- controller/delete/deleteTransaction.js
+++ controller/delete/deleteTransaction.js
@@ -3,7 +3,7 @@ import { sql } from "../../database";
 export const daleteTransaction = async (request, response) => {
   const { id } = request.body;
   try {
-    await sql`DELETE * FROM transactions where id=${id}`;
+    await sql`DELETE FROM transactions where id=${id}`;
     response.status(200).json({ message: "amjilttai ustgalaa" });
   } catch (error) {
     response.status(400).json({ message: error });
